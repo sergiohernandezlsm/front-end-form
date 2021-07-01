d3fcffe12d82cb9f351001d6bd3f923be5cf1834
Sergio <sergio.hernandez@littlestarmedia.com>
2021-07-01 15:53:27 +0100

    first commit

---
+++ src/components/List/List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const appForm = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+)}
+
+export default appForm;
